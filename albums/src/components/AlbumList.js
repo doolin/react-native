78263daa4b6c28e6c59fe5f9709d89fa6460db8a
dave doolin <david.doolin@gmail.com>
2018-08-26 07:03:01 -0700

    video 31 change album list to class-based

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
+class AlbumList extends Component {
+    render() {
   return (
       <View>
           <Text>Album list</Text>
       </View>
   );
-};
+}
+}
 
 export default AlbumList;
