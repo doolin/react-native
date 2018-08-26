69cc799f0f8ccb8cd18df2d282b1978861e50e1c
dave doolin <david.doolin@gmail.com>
2018-08-26 13:37:43 -0700

    video 37 render list of album titles

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -12,11 +12,15 @@ class AlbumList extends Component {
         .then(response => this.setState({ albums: response.data }));
     }
 
+    renderAlbums() {
+      return this.state.albums.map(album => <Text>{album.title}</Text>);
+    }
+
     render() {
         console.log(this.state);
   return (
       <View>
-          <Text>Album list</Text>
+          {this.renderAlbums()}
       </View>
   );
 }
