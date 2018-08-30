8fed11079dc62f8ad3ebd4a55b8dc11d588c8585
dave doolin <david.doolin@gmail.com>
2018-08-30 04:33:56 -0700

    video 42 card section added

--- albums/index.js
+++ albums/index.js
@@ -3,7 +3,6 @@ import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
-// https://rallycoding.herokuapp.com/api/music_albums
 
 const App = () => (
     <View>
