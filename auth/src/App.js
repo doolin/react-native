b0930cdf5235c5d1c5334e5addec2bbac2e70b47
dave doolin <david.doolin@gmail.com>
2018-09-11 03:47:30 -0700

    video 59, start of firebase auth

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: ""
+          }
+        )
+    }
+
     render() {
         return (
             <View>
