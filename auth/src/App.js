fbca88c2e85008d97fd48066a23878a3f9db3e95
dave doolin <david.doolin@gmail.com>
2018-09-12 03:43:26 -0700

    video 60, login form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
 import firebase from 'firebase';
+
+import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
+
 
 class App extends Component {
     componentWillMount() {
@@ -20,7 +23,7 @@ class App extends Component {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm />
             </View>
         );
     }
