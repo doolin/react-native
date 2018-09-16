1e6f64a87d12ae8327f914c3ea665feadc2992f6
dave doolin <david.doolin@gmail.com>
2018-09-16 08:07:00 -0700

    video 64, input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import { View, TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
-    state = { text; '' };
+    state = { text: '' };
 
     render () {
         return (
