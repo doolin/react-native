6e9c1613466d3954c7c17a85447a844149a9379b
dave doolin <david.doolin@gmail.com>
2018-10-17 04:34:56 -0700

    video 98, hook up reducer

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
     const { titleStyle } = styles;
+    const { id, title } = this.props.library;
     console.log(props);
 
     render() {
         return (
-            <CardSection>
+            <TouchableWithoutFeedback
+              onPress={() => this.props.selectLibrary(id)}
+            >
+              <View>
+              <CardSection>
                 <Text style={titleStyle}>
-                    {this.props.library.title}
+                  {title}
                 </Text>
-            </CardSection>
+              </CardSection>
+              </View>
+            </TouchableWithoutFeedback>
         );
 
     }
