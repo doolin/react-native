f1eba822a64d000ba47b57fa79b6bd0030a11bf2
dave doolin <david.doolin@gmail.com>
2018-11-06 05:19:40 -0800

    video 100, still borked

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -5,11 +5,21 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
-    const { titleStyle } = styles;
-    const { id, title } = this.props.library;
-    console.log(props);
+    renderDescription() {
+        const { library, selectedLibraryId } = this.props;
+         
+        if (library.id === selectedLibraryId) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
 
     render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+        console.log(props);
+
         return (
             <TouchableWithoutFeedback
               onPress={() => this.props.selectLibrary(id)}
@@ -20,6 +30,7 @@ class ListItem extends Component {
                   {title}
                 </Text>
               </CardSection>
+              {this.renderDescription}
               </View>
             </TouchableWithoutFeedback>
         );
@@ -34,4 +45,8 @@ const styles = {
     }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId }
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
