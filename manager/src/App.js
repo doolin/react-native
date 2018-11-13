a163765d3d7ec821ffd59037e27f5c370dd330d4
dave doolin <david.doolin@gmail.com>
2018-11-13 04:16:34 -0800

    video 108, firebase creds minus api key

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,23 @@ import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+
 import reducers from './reducers';
 
-
-
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "", // copy in API key.
+      authDomain: "manager-d8bc7.firebaseapp.com",
+      databaseURL: "https://manager-d8bc7.firebaseio.com",
+      projectId: "manager-d8bc7",
+      storageBucket: "manager-d8bc7.appspot.com",
+      messagingSenderId: "955462005742"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
